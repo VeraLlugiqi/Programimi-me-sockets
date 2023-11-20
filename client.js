dd582766515cd1fa13f1f7b498925df501ad680a
valtrinacacaj <118756985+valtrinacacaj@users.noreply.github.com>
2023-11-20 14:00:47 +0100

    Update client.js

---
+++ client.js
@@ -0,0 +1,17 @@
+const rl = readline.createInterface({ 
+
+input: process.stdin 
+
+}); 
+
+rl.on('line', (line) => { 
+
+client.write(${line}\n); 
+
+}); 
+
+rl.on('close', () => { 
+
+client.end(); 
+
+});
