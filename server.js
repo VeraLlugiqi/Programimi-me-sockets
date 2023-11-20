98e3f9d665351e616935331730e639e0d4fb9541
Suhejla <121345932+SuhejlaHoxha@users.noreply.github.com>
2023-11-20 14:01:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const net = require('net');
+const fs = require('fs');
+const { exec } = require("child_process");
 const PORT = 3000;
 
+//server creation and message from which address and port is connection made
+const server = net.createServer((socket) => {
+    console.log(
+        'Connection from',
+        socket.remoteAddress,
+        'port',
+        socket.remotePort
+    );
 server.on('error',function(error){
   console.log('Error: ' + error);
   server.close();
