c8f33ba98cae181c0902a60361a4b8026d2adece
Vera Llugiqi <vera.llugiqi@student.uni-pr.edu>
2023-11-22 23:21:35 +0100

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const net = require('net');
 const fs = require('fs');
 const { exec } = require("child_process");
-const PORT = 3000;
+const PORT = 6452;
 
-//server creation and message from which address and port is connection made
+//krijimi i serverit dhe mesazhit nga e cila adresa dhe porti eshte konektuar
 const server = net.createServer((socket) => {
     console.log(
         'Connection from',
