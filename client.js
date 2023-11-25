1cb597330cf1e1b81b410c758024c72bf83c9551
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-11-25 23:29:24 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,3 +1,24 @@
+const net = require('net'); 
+
+const readline = require('readline'); 
+
+//krijimi i nje client socket te ri dhe konektimi me serverin duke perdorur portin e njejte me serverin
+const client = new net.Socket(); 
+
+client.connect(58901, process.argv[2], () => { 
+
+console.log('Connected to server'); 
+
+console.log("If u want to get your access to write read or execute please type 'login' "); 
+
+});
+
+client.on('data', (data) => { 
+
+console.log(data.toString('utf-8')); 
+
+}); 
+
 const rl = readline.createInterface({ 
 
   input: process.stdin 
