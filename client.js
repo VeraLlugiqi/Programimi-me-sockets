2f1f5c3968b91efb51fc5f114f89ba5d8c62838c
valtrinacacaj <118756985+valtrinacacaj@users.noreply.github.com>
2023-11-29 14:41:53 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,38 +1,28 @@
-const net = require('net'); 
+const net = require('net');
+const readline = require('readline');
 
-const readline = require('readline'); 
+// Replace 'SERVER_IP' with the actual IP address of your server
+const serverIp = '10.103.107.201';
 
-//krijimi i nje client socket te ri dhe konektimi me serverin duke perdorur portin e njejte me serverin
-const client = new net.Socket(); 
-
-client.connect(58901, process.argv[2], () => { 
-
-console.log('Connected to server'); 
-
-console.log("If u want to get your access to write read or execute please type 'login' "); 
-
+const client = new net.Socket();
+var port = 58901;
+client.connect(port, serverIp, () => {
+  console.log('U lidhet me server');
+  console.log("Shkruani login per te pasur casje per read, write or execute");
 });
 
-client.on('data', (data) => { 
+client.on('data', (data) => {
+  console.log(data.toString('utf-8'));
+});
 
-console.log(data.toString('utf-8')); 
+const rl = readline.createInterface({
+  input: process.stdin
+});
 
-}); 
+rl.on('line', (line) => {
+  client.write(`${line}\n`);
+});
 
-const rl = readline.createInterface({ 
-
-  input: process.stdin 
-
-  }); 
-
-  rl.on('line', (line) => { 
-
-  client.write(${line}\n); 
-
-  }); 
-
-  rl.on('close', () => { 
-
-  client.end(); 
-
+rl.on('close', () => {
+  client.end();
 });
